8dffab108a2ec99865beb4ee2eba3d3b04bea86c
Mohmd20 <mohammad.hfarrahi@gmail.com>
2025-02-04 12:04:05 +0330

    feat report to admin

--- script.py
+++ script.py
@@ -56,7 +56,7 @@ async def table_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [[InlineKeyboardButton(COLUMN_LABELS.get(col, col), callback_data=f"column_{col}")] for col in columns]
     keyboard.append([InlineKeyboardButton("بازگشت", callback_data="back_to_tables")])
     reply_markup = InlineKeyboardMarkup(keyboard)
-    await query.edit_message_text(f"شما {TABLE_LABELS.get(table_key, table_key)} را انتخاب کردید.\n هر کدوم رو خواستی انتخاب کن تا کاتالوگش رو برات ارسال کنم", reply_markup=reply_markup)
+    await query.edit_message_text(f"شما {TABLE_LABELS.get(table_key, table_key)} را انتخاب کردید.\nهر کدام را خواستید انتخاب کنید تا کاتالوگش را براتون ارسال کنم", reply_markup=reply_markup)
 async def support (update:Update , context:ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("برای ارتباط با ما روی   id زیر کلیک کنید :(https://t.me/misterwebdeveloper)" , parse_mode="Markdown")
 async def set_bot_commands(application: Application):
